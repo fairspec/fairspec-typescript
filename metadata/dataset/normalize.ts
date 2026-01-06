51149a4b92070d79df48fd03e85f9aee9cbf5581
roll <eskarev@gmail.com>
2026-01-06 09:47:08 +0000

    Fixed dataset normalization

--- metadata/dataset/normalize.ts
+++ metadata/dataset/normalize.ts
@@ -1,5 +1,7 @@
 import type { Descriptor } from "../descriptor/index.ts"
+import { normalizeJsonSchema } from "../jsonSchema/index.ts"
 import { normalizePath } from "../path/index.ts"
+import { normalizeTableSchema } from "../tableSchema/index.ts"
 
 export function normalizeDataset(
   descriptor: Descriptor,
@@ -8,11 +10,20 @@ export function normalizeDataset(
   },
 ) {
   descriptor = globalThis.structuredClone(descriptor)
-  normalizeResourcePaths(descriptor, options)
+
+  if (Array.isArray(descriptor.resources)) {
+    // TODO: why resource is any here?
+    for (const resource of descriptor.resources) {
+      normalizeResourceData(resource, options)
+      normalizeResourceJsonSchema(resource, options)
+      normalizeResourceTableSchema(resource, options)
+    }
+  }
+
   return descriptor
 }
 
-function normalizeResourcePaths(
+function normalizeResourceData(
   descriptor: Descriptor,
   options?: { basepath?: string },
 ) {
@@ -27,12 +38,34 @@ function normalizeResourcePaths(
       descriptor.path[index] = normalizePath(path, { basepath })
     }
   }
+}
 
-  for (const name of ["dialect", "schema"] as const) {
-    if (typeof descriptor[name] === "string") {
-      descriptor[name] = normalizePath(descriptor[name], {
-        basepath,
-      })
-    }
+function normalizeResourceJsonSchema(
+  descriptor: Descriptor,
+  options?: { basepath?: string },
+) {
+  const basepath = options?.basepath
+
+  if (typeof descriptor.jsonSchema === "string") {
+    descriptor.jsonSchema = normalizePath(descriptor.jsonSchema, { basepath })
+  } else if (descriptor.jsonSchema) {
+    // TODO: fix
+    // @ts-expect-error
+    descriptor.jsonSchema = normalizeJsonSchema(descriptor.jsonSchema)
+  }
+}
+
+function normalizeResourceTableSchema(
+  descriptor: Descriptor,
+  options?: { basepath?: string },
+) {
+  const basepath = options?.basepath
+
+  if (typeof descriptor.tableSchema === "string") {
+    descriptor.tableSchema = normalizePath(descriptor.tableSchema, { basepath })
+  } else if (descriptor.tableSchema) {
+    // TODO: fix
+    // @ts-expect-error
+    descriptor.tableSchema = normalizeTableSchema(descriptor.tableSchema)
   }
 }

bc625be3ce84b0e95697a857285a3ef19f9c54b5
roll <eskarev@gmail.com>
2026-01-10 13:39:27 +0000

    Support integer -> boolean narrowing

--- table/actions/column/narrow.ts
+++ table/actions/column/narrow.ts
@@ -4,6 +4,17 @@ import type { ColumnMapping } from "../../models/column.ts"
 export function narrowColumn(mapping: ColumnMapping, columnExpr: pl.Expr) {
   const variant = mapping.source.type.variant
 
+  if (mapping.target.type === "boolean") {
+    if (["Int8", "Int16", "Int32", "Int64"].includes(variant)) {
+      columnExpr = pl
+        .when(columnExpr.eq(1))
+        .then(pl.lit(true))
+        .when(columnExpr.eq(0))
+        .then(pl.lit(false))
+        .otherwise(pl.lit(null))
+    }
+  }
+
   if (mapping.target.type === "integer") {
     if (["Float32", "Float64"].includes(variant)) {
       columnExpr = pl

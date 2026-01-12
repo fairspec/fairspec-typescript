3a038fbec844b03016bacaff7955d8df9533ad36
roll <eskarev@gmail.com>
2026-01-12 13:35:44 +0000

    Fixed dateTime spec

--- table/actions/column/types/dateTime.spec.ts
+++ table/actions/column/types/dateTime.spec.ts
@@ -1,10 +1,9 @@
+import type { TableSchema } from "@fairspec/metadata"
 import * as pl from "nodejs-polars"
 import { describe, expect, it } from "vitest"
 import { denormalizeTable } from "../../../actions/table/denormalize.ts"
 import { normalizeTable } from "../../../actions/table/normalize.ts"
 
-// TODO: Enable this test suite after this issue is fixed:
-// https://github.com/pola-rs/nodejs-polars/issues/365
 describe.skip("parseDatetimeColumn", () => {
   it.each([
     // Default format
@@ -18,25 +17,32 @@ describe.skip("parseDatetimeColumn", () => {
     [
       "21/11/2006 16:30",
       new Date(2006, 10, 21, 16, 30),
-      { format: "%d/%m/%Y %H:%M" },
+      { temporalFormat: "%d/%m/%Y %H:%M" },
     ],
-    ["16:30 21/11/06", null, { format: "%H:%M %d/%m/%y" }], // Incorrect format
-    ["invalid", null, { format: "%d/%m/%y %H:%M" }],
-    ["", null, { format: "%d/%m/%y %H:%M" }],
+    ["16:30 21/11/06", null, { temporalFormat: "%H:%M %d/%m/%y" }],
+    ["invalid", null, { temporalFormat: "%d/%m/%y %H:%M" }],
+    ["", null, { temporalFormat: "%d/%m/%y %H:%M" }],
 
     // Invalid format
-    ["21/11/06 16:30", null, { format: "invalid" }],
+    ["21/11/06 16:30", null, { temporalFormat: "invalid" }],
   ])("%s -> %s %o", async (cell, expected, options) => {
     const table = pl.DataFrame([pl.Series("name", [cell], pl.String)]).lazy()
 
-    const schema = {
-      columns: [{ name: "name", type: "datetime" as const, ...options }],
+    const tableSchema: TableSchema = {
+      properties: {
+        name: {
+          type: "string",
+          format: "date-time",
+          ...options,
+        },
+      },
     }
 
-    const result = await normalizeTable(table, schema)
+    const result = await normalizeTable(table, tableSchema)
     const frame = await result.collect()
 
-    expect(frame.toRecords()[0]?.name).toEqual(expected)
+    const actual = frame.toRecords()[0]?.name
+    expect(actual).toEqual(expected)
   })
 })
 
@@ -50,23 +56,30 @@ describe("stringifyDatetimeColumn", () => {
     [
       new Date(Date.UTC(2006, 10, 21, 16, 30, 0)),
       "21/11/2006 16:30",
-      { format: "%d/%m/%Y %H:%M" },
+      { temporalFormat: "%d/%m/%Y %H:%M" },
     ],
     [
       new Date(Date.UTC(2014, 0, 1, 6, 0, 0)),
       "2014/01/01T06:00:00",
-      { format: "%Y/%m/%dT%H:%M:%S" },
+      { temporalFormat: "%Y/%m/%dT%H:%M:%S" },
     ],
   ])("%s -> %s %o", async (value, expected, options) => {
     const table = pl.DataFrame([pl.Series("name", [value], pl.Datetime)]).lazy()
 
-    const schema = {
-      columns: [{ name: "name", type: "datetime" as const, ...options }],
+    const tableSchema: TableSchema = {
+      properties: {
+        name: {
+          type: "string",
+          format: "date-time",
+          ...options,
+        },
+      },
     }
 
-    const result = await denormalizeTable(table, schema)
+    const result = await denormalizeTable(table, tableSchema)
     const frame = await result.collect()
 
-    expect(frame.toRecords()[0]?.name).toEqual(expected)
+    const actual = frame.toRecords()[0]?.name
+    expect(actual).toEqual(expected)
   })
 })

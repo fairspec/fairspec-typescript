aecb246a920cc3aec29f6a09b8d03f313b4b6138
roll <eskarev@gmail.com>
2026-01-16 16:03:36 +0000

    Updated validateDataset

--- terminal/commands/dataset/index.ts
+++ terminal/commands/dataset/index.ts
@@ -3,7 +3,7 @@ import { helpConfiguration } from "../../helpers/help.ts"
 import { copyDatasetCommand } from "./copy.ts"
 import { inferDatasetCommand } from "./infer.ts"
 // import { scriptDatasetCommand } from "./script.ts"
-// import { validateDatasetCommand } from "./validate.ts"
+import { validateDatasetCommand } from "./validate.ts"
 
 export const datasetCommand = new Command("dataset")
   .configureHelp(helpConfiguration)
@@ -11,5 +11,5 @@ export const datasetCommand = new Command("dataset")
 
   .addCommand(copyDatasetCommand)
   .addCommand(inferDatasetCommand)
-// .addCommand(scriptDatasetCommand)
-// .addCommand(validateDatasetCommand)
+  // .addCommand(scriptDatasetCommand)
+  .addCommand(validateDatasetCommand)
--- terminal/commands/dataset/validate.ts
+++ terminal/commands/dataset/validate.ts
@@ -1,56 +1,28 @@
-import { validatePackage } from "@dpkit/library"
+import { validateDataset } from "@fairspec/library"
 import { Command } from "commander"
-import React from "react"
-import { Report } from "../../components/Report/index.ts"
-import { selectErrorResource, selectErrorType } from "../../helpers/error.ts"
 import { helpConfiguration } from "../../helpers/help.ts"
 import * as params from "../../params/index.ts"
-import { createSession, Session } from "../../session.ts"
+import { Session } from "../../session.ts"
 
 export const validateDatasetCommand = new Command("validate")
   .configureHelp(helpConfiguration)
-  .description("Validate a data package from a local or remote path")
+  .description("Validate a dataset from a local or remote path")
 
   .addArgument(params.positionalDescriptorPath)
   .addOption(params.json)
   .addOption(params.debug)
-  .addOption(params.quit)
-  .addOption(params.all)
 
   .action(async (path, options) => {
-    const session = createSession({
-      title: "Validate package",
-      json: options.json,
+    const session = new Session({
       debug: options.debug,
-      quit: options.quit,
-      all: options.all,
+      json: options.json,
     })
 
-    const report = await session.task(
-      "Validating package",
-      validatePackage(path),
-    )
+    const report = await session.task("Validating dataset", async () => {
+      return await validateDataset(path)
+    })
 
-    if (report.errors.length) {
-      // @ts-expect-error
-      const name = await selectErrorResource(session, report.errors)
-      // @ts-expect-error
-      if (name) report.errors = report.errors.filter(e => e.resource === name)
+    // TODO: Support error rendering
 
-      // @ts-expect-error
-      const type = await selectErrorType(session, report.errors)
-      // @ts-expect-error
-      if (type) report.errors = report.errors.filter(e => e.type === type)
-    }
-
-    if (report.valid) {
-      session.success("Package is valid")
-      return
-    }
-
-    session.render(
-      report,
-      // @ts-expect-error
-      <Report errors={report.errors} quit={options.quit} />,
-    )
+    session.renderDataResult(report)
   })

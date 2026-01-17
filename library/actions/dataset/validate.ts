ea66cb7db29a21f3ebedce58286ea169853aa444
roll <eskarev@gmail.com>
2026-01-17 12:27:12 +0000

    Simplify validateDataset

--- library/actions/dataset/validate.ts
+++ library/actions/dataset/validate.ts
@@ -1,61 +1,14 @@
 import os from "node:os"
-import type { Dataset, DatasetError, Descriptor } from "@fairspec/metadata"
-import {
-  createReport,
-  loadDescriptor,
-  resolveBasepath,
-  validateDatasetDescriptor,
-} from "@fairspec/metadata"
+import type { Dataset, DatasetError } from "@fairspec/metadata"
+import { createReport } from "@fairspec/metadata"
 import pAll from "p-all"
 import { validateResource } from "../../actions/resource/validate.ts"
-import { system } from "../../system.ts"
 import { validateDatasetIntegrity } from "./integrity.ts"
 
-export async function validateDataset(
-  source: string | Descriptor | Dataset,
-  options?: { basepath?: string },
-) {
-  let descriptor: Descriptor | undefined
-  let basepath = options?.basepath
-
-  if (typeof source !== "string") {
-    descriptor = source
-  } else {
-    for (const plugin of system.plugins) {
-      const result = await plugin.loadDataset?.(source)
-      if (result) {
-        descriptor = result as unknown as Descriptor
-        break
-      }
-    }
-
-    if (!descriptor) {
-      basepath = await resolveBasepath(source)
-      descriptor = await loadDescriptor(source)
-    }
-  }
-
-  const descriptorReport = await validateDatasetDescriptor(descriptor, {
-    basepath,
-  })
-
-  if (!descriptorReport.dataset) {
-    return {
-      valid: descriptorReport.valid,
-      errors: descriptorReport.errors.map(error => ({
-        ...error,
-        resource: undefined,
-      })),
-    }
-  }
-
-  const resourcesReport = await validateDatasetResources(
-    descriptorReport.dataset,
-  )
-
-  const integrityReport = await validateDatasetIntegrity(
-    descriptorReport.dataset,
-  )
+export async function validateDataset(dataset: Dataset) {
+  // TODO: parallelize?
+  const resourcesReport = await validateDatasetResources(dataset)
+  const integrityReport = await validateDatasetIntegrity(dataset)
 
   const errors = [...resourcesReport.errors, ...integrityReport.errors]
   return createReport(errors)

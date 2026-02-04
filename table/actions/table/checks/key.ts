4203836243e30f520b17ebc9051ed95cc1e0e871
roll <eskarev@gmail.com>
2026-02-04 13:26:47 +0000

    Remove duplication from key check

--- table/actions/table/checks/key.ts
+++ table/actions/table/checks/key.ts
@@ -2,21 +2,26 @@ import type { RowPrimaryKeyError, RowUniqueKeyError } from "@fairspec/metadata"
 import * as pl from "nodejs-polars"
 import type { SchemaMapping } from "../../../models/schema.ts"
 
+type KeyCheckType = "primary" | "unique"
+
 export function createRowKeyChecks(mapping: SchemaMapping) {
   const uniqueKeys = mapping.target.uniqueKeys ?? []
   const primaryKey = mapping.target.primaryKey
 
   const checks = [
-    ...(primaryKey ? [createCheckRowPrimaryKey(primaryKey)] : []),
-    ...uniqueKeys.map(createCheckRowUniqueKey),
+    ...(primaryKey
+      ? [createRowKeyCheck(primaryKey, { keyType: "primary" })]
+      : []),
+    ...uniqueKeys.map(key => createRowKeyCheck(key, { keyType: "unique" })),
   ]
 
   return checks
 }
 
-// TODO: Remove duplication
-
-function createCheckRowPrimaryKey(uniqueKey: string[]) {
+function createRowKeyCheck(
+  uniqueKey: string[],
+  options: { keyType: KeyCheckType },
+) {
   const isErrorExpr = pl
     .concatList(uniqueKey)
     .isFirstDistinct()
@@ -24,25 +29,8 @@ function createCheckRowPrimaryKey(uniqueKey: string[]) {
     // Fold is not available so we use a tricky way to eliminate nulls
     .and(pl.concatList(uniqueKey).lst.min().isNotNull())
 
-  const errorTemplate: RowPrimaryKeyError = {
-    type: "row/primaryKey",
-    columnNames: uniqueKey,
-    rowNumber: 0,
-  }
-
-  return { isErrorExpr, errorTemplate }
-}
-
-function createCheckRowUniqueKey(uniqueKey: string[]) {
-  const isErrorExpr = pl
-    .concatList(uniqueKey)
-    .isFirstDistinct()
-    .not()
-    // Fold is not available so we use a tricky way to eliminate nulls
-    .and(pl.concatList(uniqueKey).lst.min().isNotNull())
-
-  const errorTemplate: RowUniqueKeyError = {
-    type: "row/uniqueKey",
+  const errorTemplate: RowPrimaryKeyError | RowUniqueKeyError = {
+    type: options.keyType === "primary" ? "row/primaryKey" : "row/uniqueKey",
     columnNames: uniqueKey,
     rowNumber: 0,
   }
